c5f52e078cb317586f77c2479a61129569787e34
Nicholas Pritchyk <nickpritchyk20@gmail.com>
2023-06-28 00:47:51 -0600

    added more cart details

--- src/pages/CartPage.jsx
+++ src/pages/CartPage.jsx
@@ -10,25 +10,32 @@ export default function CartPage(props) {
 
     console.log(numItems)
     return(
-        <div className="cart-page">
-        <div className="cart">
-            <div className='card-deck'>
-            { BOATDATA.map((res) => {
-                if(cart[res.id] !== 0) {
-                    return <CartCard data={res}></CartCard>
-                } else return null
-            })}
-            </div>
-            { (numItems) === 0 && 
-                <div className='empty-cart'>
-                    <p> Your cart is empty. </p>
-                    <Link style={{color: '#038aff', fontSize: 'large'}} className='empty-cart' to='/shop'> Start Shopping </Link>
+        <div className='container'>
+            <h1> Your Cart ({ numItems })</h1>
+            <div className="cart-page">
+                <div className="cart">
+                    <div className='card-deck'>
+                    { BOATDATA.map((res) => {
+                        if(cart[res.id] !== 0) {
+                            return <CartCard data={res}></CartCard>
+                        } else return null
+                    })}
+                    </div>
+                    { (numItems) === 0 && 
+                        <div className='empty-cart'>
+                            <p> Your cart is empty. </p>
+                            <Link style={{color: '#038aff', fontSize: 'large'}} className='empty-cart' to='/shop'> Start Shopping </Link>
+                        </div>
+                    }
                 </div>
-            }
-        </div>
-        <div className='cart-info'>
-            <p> { getTotalPrice() }</p>
-        </div>
+                <div className='cart-info'>
+                    <p>  Subtotal: ${ getTotalPrice().toLocaleString() }</p>
+                    <p>  Tax: ${ ((getTotalPrice())*0.18).toLocaleString() }</p>
+                    <p>  Promo Code: <input placeholder='Enter promo code'></input> <button> Submit </button></p>
+                    <p>  Total: ${ ((getTotalPrice())*0.18 + (getTotalPrice())).toLocaleString() }</p>
+                    <button> Checkout </button>
+                </div>
+            </div>
         </div>
     )
 }
